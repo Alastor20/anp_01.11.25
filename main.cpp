6b00fa0470745c5748bb03ddadddcfbe229edcf5
Alastor20 <nik_d.007@mail.ru>
2025-11-01 12:26:45 +0300

    init matrix

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
+int **make_mtx(int r, int c);
+
 int main()
 {
-    long long a = 0, b = 0;
-    std::cin >> a >> b;
+    long long rows = 0, cols = 0;
+    std::cin >> rows >> cols;
     if (std::cin.fail())
     {
         std::cerr << "ERROR: cant read\n";
         return 1;
     }
+    int **mtx = nullptr;
+    mtx = make_mtx(rows, cols);
 }
