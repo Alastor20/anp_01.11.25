3320a9ef91622b705d2a42753e0f83cde6fe8fe5
Alastor20 <nik_d.007@mail.ru>
2025-11-01 12:19:20 +0300

    enter nums and handel error

--- main.cpp
+++ main.cpp
@@ -1,2 +1,12 @@
+#include <iostream>
+
 int main()
-{}
+{
+    long long a = 0, b = 0;
+    std::cin >> a >> b;
+    if (std::cin.fail())
+    {
+        std::cerr << "ERROR: cant read\n";
+        return 1;
+    }
+}
